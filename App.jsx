84aadf93cdca2992ab0ebc2da9a4f2af560536b8
zebedee314 <zockolzj@gmail.com>
2026-03-09 15:37:38 -0400

    Create App.jsx file for MTG Collection Hunter React app

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>MTG Collection Hunter</h1>
+      {/* Your code here */}
+    </div>
+  );
+};
+
+export default App;
